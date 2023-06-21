3773679b10d485c96a905941127adca39b48acba
Tanya N <134557318+tnashconsult@users.noreply.github.com>
2023-06-21 17:42:13 +0000

    Update Main() method and dev container

--- Fraction.java
+++ Fraction.java
@@ -3,17 +3,30 @@ import java.util.Scanner;
 public class Fraction {
 
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int numerator, denominator, result;
-    
-    System.out.println("Enter Numerator");
-    numerator = scanner.nextInt();
-    System.out.println("Enter Denominator:");
-    denominator = scanner.nextInt();
-    System.out.println();
 
-    result = numerator/denominator;
-    System.out.println( numerator + " / " + denominator + " = " + result);
+    Scanner scanner = null;
+    
+    try{
+      scanner = new Scanner(System.in);
+      int numerator, denominator, result;
+        
+      System.out.println("Enter Numerator");
+      numerator = scanner.nextInt();
+      System.out.println("Enter Denominator:");
+      denominator = scanner.nextInt();
+      System.out.println();
+
+      result = numerator/denominator;
+      System.out.println( numerator + " / " + denominator + " = " + result);
+
+    }
+    finally{
+
+      if(scanner!=null)
+        scanner.close();
+
+
+    }
     
   }
 
